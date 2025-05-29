ac962a2e2dec7a01829dd20fa6fbc3cece4a2b12
ManuelSD <elnuma82@gmail.com>
2025-05-29 11:59:20 +0200

    creacion cursos

---
+++ resources/views/cursos/index.blade.php
@@ -0,0 +1,49 @@
+@extends('template.base')
+
+@section('title', 'Cursos')
+
+@section('content')
+    <h1>Cursos</h1>
+
+    <a href="{{ route('cursos.create') }}" class="btn btn-primary mb-3">Crear curso</a>
+
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Descripción</th>
+                <th>Fecha Inicio</th>
+                <th>Fecha Fin</th>
+                <th>Plazas</th>
+                <th>Estado</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($cursos as $curso)
+                <tr>
+                    <td>{{ $curso->id }}</td>
+                    <td>{{ $curso->titulo }}</td>
+                    <td>{{ $curso->descripcion }}</td>
+                    <td>{{ $curso->fechaInicio }}</td>
+                    <td>{{ $curso->fechaFin }}</td>
+                    <td>{{ $curso->plazas }}</td>
+                    <td>{{ $curso->estado }}</td>
+                    <td>
+                        <a href="{{ route('cursos.edit', $curso->id) }}" class="btn btn-sm btn-warning">Modificar</a>
+                        <form action="{{ route('cursos.destroy', $curso->id) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-sm btn-danger" onclick="return confirm('¿Seguro que deseas eliminar este curso?')">Eliminar</button>
+                        </form>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="8">No hay cursos disponibles.</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CursoController;
 
 Route::view('/', 'welcome');
 
@@ -24,3 +25,16 @@ Route::view('profile', 'profile')
     ->name('profile');
 
 require __DIR__.'/auth.php';
+
+// Rutas de Cursos Publicas
+Route::get('/cursos', [CursoController::class, 'index'])->name('cursos.index');
+Route::get('/cursos/create', [CursoController::class, 'create'])->name('cursos.create');
+Route::post('/cursos', [CursoController::class, 'store'])->name('cursos.store');
+
+// El resto de rutas protegidas
+Route::middleware(['auth'])->group(function () {
+    Route::get('/cursos/{id}', [CursoController::class, 'show'])->name('cursos.show');
+    Route::get('/cursos/{curso}/edit', [CursoController::class, 'edit'])->name('cursos.edit');
+    Route::put('/cursos/{curso}', [CursoController::class, 'update'])->name('cursos.update');
+    Route::delete('/cursos/{curso}', [CursoController::class, 'destroy'])->name('cursos.destroy');
+});
